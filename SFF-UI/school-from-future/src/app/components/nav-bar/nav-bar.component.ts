6d53b5793337df8b2bc452cb97a600071a487cbf
Polina Todorova <p.v.todorova@abv.bg>
2018-12-27 21:34:38 +0200

    Edit nav-bar component

--- SFF-UI/school-from-future/src/app/components/nav-bar/nav-bar.component.ts
+++ SFF-UI/school-from-future/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,6 +11,25 @@ export class NavBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  isLogged() {
+    return sessionStorage.getItem('user')!==null;
+  }
+  
+  isTeacher() {
+	var currentUser = JSON.parse(sessionStorage.getItem('user'));
+    return currentUser['role']=="teacher";
+  }
+  
+  isAdmin() {
+	var currentUser = JSON.parse(sessionStorage.getItem('user'));
+    return currentUser['role']=="admin";
+  }
+  
+  isStudent() {
+	var currentUser = JSON.parse(sessionStorage.getItem('user'));
+    return currentUser['role']=="student";
   }
 
 }
