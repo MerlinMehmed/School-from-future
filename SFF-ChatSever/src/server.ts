093c230326a91eed60815d1e648740de89848156
Merlin Mehmed <merlinmehmed17@gmail.com>
2019-01-02 15:28:02 +0200

    CHange url to fon awesome.

--- SFF-ChatSever/src/server.ts
+++ SFF-ChatSever/src/server.ts
@@ -4,7 +4,6 @@ import * as socketIo from 'socket.io';
 
 import { Message } from './model/message.model';
 import { User } from './model/user.model';
-import { isObject } from 'util';
 
 export class ChatServer {
     public static readonly PORT: number = 8070;
@@ -45,42 +44,20 @@ export class ChatServer {
             console.log('Running server on port %s', this.port);
         });
 
-        // this.io.on('connect', (socket: any) => {
-        //     console.log('Connected client on port %s.', this.port);
-        //     socket.on('message', (m: Message) => {
-        //         console.log('[server](message): %s', JSON.stringify(m));
-        //         this.io.emit('message', m);
-        //     });
-
-        //     socket.on('disconnect', () => {
-        //         console.log('Client disconnected');
-        //     });
-        // });
-
         this.io.on('connect', (socket: any) => {
-            // console.log(JSON.stringify(this.io))
-
             socket.on('add-user', (data: User) => {
-                console.log(data);
+                console.log(data.email + " connected");
                 this.clients[data.email] = {
                     socket: socket.id
                 };
-                console.log(this.clients);
             });
 
             socket.on('message', (m: Message) => {
                 console.log('[server](message): %s', JSON.stringify(m));
                 if (this.clients[m.to.email]) {
-                    // this.io.sockets.connected
                     this.io.to(this.clients[m.to.email].socket).emit(
                         'message', m
                     )
-                    // console.log(JSON.stringify(this.io))
-
-
-                    // this.io.clients[this.clients[m.to.email].socket].emit(
-                    //     'message', m
-                    // );
                 } else {
                     console.log("User is not logged: " + m.to.email);
                 }
